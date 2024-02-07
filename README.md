95df3bc722b8ee3c12da5e9231f7ad54763f80dd
HildaM03 <156733980+HildaM03@users.noreply.github.com>
2024-02-07 15:23:13 -0600

    widget hijo a padre

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_widget_hijo_a_padre
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_widget_hijo_a_padre/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_widget_hijo_a_padre
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
